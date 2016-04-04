a63ef7720e232620142d7b2596021a9703fea9a6
nkrim62 <nkrim62@gmail.com>
2016-04-04 11:31:37 -0700

    fixed class name

--- day_3/NoahKrim.java
+++ day_3/NoahKrim.java
@@ -7,7 +7,7 @@ import java.io.*;
 import java.util.*;
 import java.math.*;
 
-public class Solution {
+public class NoahKrim {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         HashMap<Integer, Astronaut> map = new HashMap<>();
